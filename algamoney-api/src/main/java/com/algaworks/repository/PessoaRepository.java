82d669e6f1045933b976b69308277e05ed504ca8
Felipe Santos <felipe.santos11@hotmail.com>
2020-04-16 08:42:51 -0300

    implementando o jwt no security da API

--- algamoney-api/src/main/java/com/algaworks/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.algaworks.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
-}
